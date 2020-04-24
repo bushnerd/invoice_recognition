e699360e0c3f8592b3b843938b833784525c39ea
ryantu <scutxd@gmail.com>
2020-04-24 22:24:08 +0800

    修改 host 地址

--- client/config.js
+++ client/config.js
@@ -4,7 +4,7 @@
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
 // var host = 'https://aduls9yo.qcloud.la';
-var host = "http://192.168.2.50:5757";
+var host = "http://192.168.2.49:5757";
 
 var config = {
   // 下面的地址配合云端 Demo 工作
@@ -21,8 +21,8 @@ var config = {
     tunnelUrl: `${host}/weapp/tunnel`,
 
     // 上传图片接口
-    uploadUrl: `${host}/weapp/upload`
-  }
+    uploadUrl: `${host}/weapp/upload`,
+  },
 };
 
 module.exports = config;
